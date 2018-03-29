90e96791efbe348c76833938527cfbd6183c403d
DESKTOP-LE53BNL\Patrick <plp248@nyu.edu>
2018-03-29 10:49:08 -0400

    simple level geometry + very simple enemy AI

---
+++ Nothing Personal/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+	public float speed;
+	public float dir = 1;
+
+	void Start()
+	{
+		speed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate(new Vector3(1,0,0) * Time.deltaTime * dir * speed);
+		
+	}
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.gameObject.tag == "Wall")
+		{
+			dir *= -1;
+		}
+	}
+}
