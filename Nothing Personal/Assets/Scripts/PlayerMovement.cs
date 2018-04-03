d801c8df715e8941a9433aef211ccaa590658de0
DESKTOP-LE53BNL\Patrick <plp248@nyu.edu>
2018-04-03 10:05:59 -0400

    very simple TP mechanic

--- Nothing Personal/Assets/Scripts/PlayerMovement.cs
+++ Nothing Personal/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 7.0f; //movement speed
+	public float tpDistance;
 	
 	// Use this for initialization
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked; //hides cursor when game plays
+		tpDistance = 10;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,19 @@ public class PlayerMovement : MonoBehaviour {
 		float forwardMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 		float sideMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		transform.Translate(sideMove, 0, forwardMove);
+	}
+
+	void OnTriggerStay(Collider col)
+	{
+		if (col.gameObject.CompareTag("FOV"))
+		{
+			
+			Debug.Log("in FOV");
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				transform.Translate(0,0,tpDistance);
+				Debug.Log("tp");
+			}
+		}
 	}
 }
