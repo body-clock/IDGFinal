f7dc82a157d8fa9db9c31b14d1a1241bc1fa792a
Patrick <plp248@nyu.edu>
2018-04-17 11:33:56 -0400

    simple start screen with level select

---
+++ Nothing Personal/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+
+	private Object[] LevelButtonList;
+	public GameObject buttonPrefab;
+	public GameObject canvas;
+
+	void Start()
+	{
+		LevelButtonList = Resources.LoadAll("Levels");
+		Debug.Log(LevelButtonList.Length);
+		
+		LoadLevels();
+	}
+
+	public void LoadLevels()
+	{
+		foreach (var l in LevelButtonList)
+		{
+			GameObject button = Instantiate(buttonPrefab);
+			button.transform.SetParent(canvas.transform);
+		}
+	}
+	
+	
+}
---
+++ Nothing Personal/Assets/Scripts/Prefs.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prefs : MonoBehaviour {
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public void NextScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+	
+}
